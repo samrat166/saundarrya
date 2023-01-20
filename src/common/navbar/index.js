231494ae430b8d9c838d8425fb7562ef1c92a2aa
samrat <samratojha16@gmail.com>
2023-01-20 12:35:30 +0545

    -new navbar

--- src/common/navbar/index.js
+++ src/common/navbar/index.js
@@ -5,35 +5,40 @@ import Logo from "../Logo";
 function Header() {
   return (
     <>
-      <Navbar bg="white" expand="md" sticky="top" className="p-0">
+      <Navbar
+        bg="transparent"
+        expand="md"
+        sticky="top"
+        className="p-0"
+        style={{ height: "75px" }}
+      >
         <Navbar.Brand href="/" className="p-0">
           <Logo small />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="me-2" />
         <Navbar.Collapse id="basic-navbar-nav" className="p-0">
-          <Nav className="ms-5 me-auto" style={{ fontSize: 20 }}>
-            <Nav.Link active href={"/gallery"}>
-              <i>Gallery</i>
+          <Nav
+            className="ms-5 me-auto"
+            style={{ fontSize: 20, fontWeight: 300 }}
+          >
+            <Nav.Link className="ms-2" active href={"/gallery"}>
+              New Arrivals
             </Nav.Link>
-            <Nav.Link href={"/products"}>
-              <i>Products</i>
+            <Nav.Link className="ms-2" href={"/products"}>
+              Clothing
             </Nav.Link>
-            <Nav.Link href={"/gallery"}>
-              <i>Latest</i>
+            <Nav.Link className="ms-2" href={"/gallery"}>
+              Dresses
             </Nav.Link>{" "}
-            <Nav.Link href={"/gallery"}>
-              <i>About Us</i>
+            <Nav.Link className="ms-2" href={"/gallery"}>
+              Bags
+            </Nav.Link>
+            <Nav.Link className="ms-2" href={"/gallery"}>
+              Sunglass
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <div
-        style={{
-          height: 0.5,
-        }}
-        className="bg-dark"
-      ></div>
     </>
   );
 }
